5682771bc666ad7d9de3f28afe1b432055734d7e
carloscacho <carlosemilio.ac@gmail.com>
2022-04-05 14:58:48 -0400

    pwa adv

---
+++ pwabuilder-adv-sw.js
@@ -0,0 +1,4 @@
+
+  import { precacheAndRoute } from 'workbox-precaching/precacheAndRoute';
+
+  precacheAndRoute([{"revision":"9b1414c912015085c864734433cc062e","url":"estilos.css"},{"revision":"43c5bf3b4dabf0735025cf95e6b946df","url":"index.html"},{"revision":"ef8b6adf77acaf6c11e7d48eae5c5cc4","url":"script.js"},{"revision":"a3777aa7da77ab821b7c7098b57d0b76","url":"sw.js"},{"revision":"b4ac4f435e55a2c81b7cb88089ca2b3b","url":"workbox-config.js"},{"revision":"5e5d3e7b1d36499930689de8a8af001c","url":"workbox-df4303c8.js"}]);
